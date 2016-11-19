8c10f229b9348c75a8024920b759555dea9e12e7
Devan Flaherty <devanflaherty@me.com>
2016-11-19 11:55:13 -0700

    Fixed excludes

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,13 +74,11 @@ $(window).scroll(function(event){
      }
    } else {
      var offset = lastScrollTop - st;
-     if(st > 300) {
-       if (offset > 4) {
-         $("#main-nav").removeClass('peek-hide');
-         $("#logoTag").removeClass('peek-hide');
-         $("#main-nav").addClass('peek');
-         $("#logoTag").addClass('peek');
-       }
+     if (offset > 2) {
+       $("#main-nav").removeClass('peek-hide');
+       $("#logoTag").removeClass('peek-hide');
+       $("#main-nav").addClass('peek');
+       $("#logoTag").addClass('peek');
      }
    }
    lastScrollTop = st;
